10fb69735095adff51461f05b7c42fa575d0b5ee
lokeshningania <lokeshningania@gmail.com>
2021-03-27 06:09:38 +0530

    changes made on Sat Mar 27 06:09:38 IST 2021

--- firstsite/views.py
+++ firstsite/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Home Page")
+    return render(request , 'home.html')
     
 def countdown(request):
     return render(request , 'countdown.html')
