7cfbcb3ea0ecc32126b4f15bf958b67e3681f389
lokeshningania <lokeshningania@gmail.com>
2021-03-29 19:32:34 +0530

    changes made on Mon Mar 29 19:32:34 IST 2021

--- searchingalgos/urls.py
+++ searchingalgos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("" , views.index , name = "Search") ,
-    path("linear",views.linearsearch , name ="linearsearch")
+    path("linear",views.linearsearch , name ="linearsearch"),
+    path("linear/performlinearsearch" , views.performlinearsearch , name="performlinearsearch")
 ]
--- searchingalgos/views.py
+++ searchingalgos/views.py
@@ -4,8 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Searching Algorithms")
+    return render(request , 'searchingalgos/index.html')
 
     
 def linearsearch(request):
-    return HttpResponse("Linear Search")
+    return render(request , 'searchingalgos/linearsearch/index.html')
+
+def performlinearsearch(request):
+    data = request.GET.get('array' , 'default')
+    print("Function working")
+    params = {'array': data}
+    return render(request , 'searchingalgos/linearsearch/result.html' , params)
